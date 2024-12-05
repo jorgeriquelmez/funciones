8847895f03d8ee5f5ed7259b31a4afbea27a7b39
jorge riquelme <jorge.riquelme.z@gmail.com>
2024-12-05 13:51:18 -0300

    se dejan solo 2 html uno para pintar y otro para las cajas

--- assets/js/4_colores.js
+++ assets/js/4_colores.js
@@ -1,3 +1,4 @@
+//cajas de  colores que cambian a negro
 const cambiaColor = (elemento, colorea) => {
     elemento.style.backgroundColor = colorea;
   }
@@ -11,3 +12,29 @@ const cambiaColor = (elemento, colorea) => {
   box2.addEventListener("click", () => cambiaColor(box2, "black"));
   box3.addEventListener("click", () => cambiaColor(box3, "black"));
   box4.addEventListener("click", () => cambiaColor(box4, "black"));
+
+//caja blanca y creacion de nuevas cajas al apretar letras
+  const key = document.getElementById("key");
+const color = {
+  a: "pink",
+  s: "orange",
+  d: "skyblue",
+  q: "purple",
+  w: "grey",
+  e: "brown",
+};
+document.addEventListener("keydown", function (event) {
+  const tecla = event.key;
+  if (tecla in color) {
+    if (tecla === "q" || tecla === "w" || tecla === "e") {
+      const nuevaCaja = document.createElement("div");
+      nuevaCaja.style.width = nuevaCaja.style.height = "200px";
+      nuevaCaja.style.marginBottom = "5px";
+      nuevaCaja.style.marginLeft = "57px";
+      nuevaCaja.style.backgroundColor = color[tecla];
+      document.body.appendChild(nuevaCaja);
+    } else if (tecla === "a" || tecla === "s" || tecla === "d") {
+      key.style.backgroundColor = color[tecla];
+    }
+  }
+});
