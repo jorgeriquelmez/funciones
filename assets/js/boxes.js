5446b61dc126e04f37c4b09dacab78eb623865f0
jorge riquelme <jorge.riquelme.z@gmail.com>
2024-12-05 12:18:53 -0300

    se agrega margin a las boxes nuevas

--- assets/js/boxes.js
+++ assets/js/boxes.js
@@ -13,6 +13,7 @@ document.addEventListener("keydown", function (event) {
     if (tecla === "q" || tecla === "w" || tecla === "e") {
       const nuevaCaja = document.createElement("div");
       nuevaCaja.style.width = nuevaCaja.style.height = "200px";
+      nuevaCaja.style.marginBottom = "5px";
       nuevaCaja.style.backgroundColor = color[tecla];
       document.body.appendChild(nuevaCaja);
     } else if (tecla === "a" || tecla === "s" || tecla === "d") {
