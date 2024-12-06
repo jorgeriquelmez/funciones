7897da49c7e38d08e936888582680669a9e8366e
jorge riquelme <jorge.riquelme.z@gmail.com>
2024-12-06 17:06:26 -0300

    correccion de fondo verde en codigo js de pintar.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 //Ejercicio Pintar
-function pintar(elemento, color = "green"){
+function pintar(elemento, color ){
   ele1.style.backgroundColor = color;
 }
-
+ele1.style.backgroundColor = 'green';
 const elemento = document.getElementById("ele1");
 elemento.addEventListener("click",() => pintar(elemento,  color = "yellow"))
 
